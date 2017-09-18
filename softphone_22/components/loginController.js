1fbc37a364c65c17f4f5d8d22600dad11a82bc6d
hanchaoj <hanchaojunjava@163.com>
2017-09-18 15:17:10 +0800

    删除

--- softphone_22/components/loginController.js
+++
@@ -1,8 +0,0 @@
-angular.module('zikerCeoApp').controller('loginController',function($scope,$http){
-  $scope.title = 'hello java';
-
-  $http.get('/user/gettest?name=wang').then(function(data){
-    $scope.data = data;
-  })
-
-})
